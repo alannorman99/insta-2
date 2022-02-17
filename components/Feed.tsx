f1e0ad621318747f080fb3d094375466ca26911e
Alan Norman <asnorman3604@eagle.fgcu.edu>
2022-02-17 17:18:21 -0500

    Finished miniprofile design. TODO: add functionality to the sign out button once auth is implemented.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,7 @@
+import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
+import Suggestions from './Suggestions'
 
 function Feed() {
   return (
@@ -11,9 +13,13 @@ function Feed() {
         <Posts />
       </section>
 
-      <section>
-        {/* Mini Profile */}
-        {/* Suggestions */}
+      <section className="hidden md:col-span-1 xl:inline-grid">
+        <div className="fixed top-20">
+          <MiniProfile />
+          {/* Mini Profile */}
+          <Suggestions />
+          {/* Suggestions */}
+        </div>
       </section>
     </main>
   )
---
+++ components/MiniProfile.tsx
@@ -0,0 +1,20 @@
+function MiniProfile() {
+  return (
+    <div className="mt-14 ml-10 flex items-center justify-between">
+      <img
+        className="h-16 w-16 rounded-full border p-[2px]"
+        src="https://links.papareact.com/3ke"
+        alt=""
+      />
+
+      <div className="mx-4 flex-1">
+        <h2 className="font-bold">alannorman00</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+      </div>
+
+      <button className="text-sm font-semibold text-blue-400">Sign Out</button>
+    </div>
+  )
+}
+
+export default MiniProfile
