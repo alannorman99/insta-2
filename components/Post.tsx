f9031956fcaffec17c94b6791bc1df3af50e3410
Alan Norman <asnorman3604@eagle.fgcu.edu>
2022-02-17 17:02:16 -0500

    Finished structure for the caption and post. TODO: Implemenent firebase background to keep track of comments and likes.

--- components/Post.tsx
+++ components/Post.tsx
@@ -25,9 +25,33 @@ function Post({ id, username, userImg, img, caption }: any) {
       {/* img */}
       <img className="w-full object-cover" src={img} alt="" />
       {/* Buttons */}
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+
       {/* caption */}
+      <div>
+        <p className="truncate p-5">
+          <span className="mr-1 font-bold">{username} </span>
+          {caption}
+        </p>
+      </div>
       {/* comments */}
       {/* input box */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Add a comment.."
+          className="flex-1 border-none outline-none focus:ring-0"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
     </div>
   )
 }
