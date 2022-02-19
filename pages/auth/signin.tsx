463cb4cf7e8122a7722b42f12a5bccac2f660d4a
Alan Norman <asnorman3604@eagle.fgcu.edu>
2022-02-19 18:16:32 -0500

    Finished sign in page and added callback url to send you to home page after sign in.

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,16 +1,34 @@
 import { getProviders, signIn as SignIntoProvider } from 'next-auth/react'
+import Header from '../../components/Header'
 
 //running on browser
 function signIn({ providers }: any) {
   return (
     <>
-      {Object.values(providers).map((provider: any) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+
+      <div className="-mt-56 flex min-h-screen flex-col items-center justify-center py-2 px-14 text-center">
+        <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+
+        <p className="font-xs italic">
+          This is not a REAL app, it is built for educational puposes only
+        </p>
+
+        <div className="mt-40">
+          {Object.values(providers).map((provider: any) => (
+            <div key={provider.name}>
+              <button
+                className="rounded-lg bg-blue-500 p-3 text-white"
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: '/' })
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   )
 }
